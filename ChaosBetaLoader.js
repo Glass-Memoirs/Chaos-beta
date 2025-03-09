1a935a6ef8407294475af643686100e0eb5e51df
Glass-Memoirs <144963518+Glass-Memoirs@users.noreply.github.com>
2025-03-09 17:56:14 -0400

    Update ChaosBetaLoader.js

--- ChaosBetaLoader.js
+++ ChaosBetaLoader.js
@@ -1,6 +1,6 @@
 document.addEventListener('corru_entered', ()=>{
     if(page.path == '/local/beneath/embassy/' || page.path == '/local/ozo/') {
-    addResources(["https://glass-memoirs.github.io/Glass-Memoirs/ECHOCLOCKHumors.js"])
+    addResources(["https://glass-memoirs.github.io/Chaos-beta/ChaosBeta.js"])
     addResources(["https://adenator.neocities.org/corrumods/literallyTooManyHumors.js"])
     }
 })
