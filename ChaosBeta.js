eb2fbaaaea7d9c228be29eac567da0f8d4190b13
Glass-Memoirs <144963518+Glass-Memoirs@users.noreply.github.com>
2025-03-19 12:18:20 -0400

    hopefully fixed this

--- ChaosBeta.js
+++ ChaosBeta.js
@@ -3044,7 +3044,7 @@ env.ACTIONS.btgothwar = {
 			user,
 			target,
 			critExec: ({target}) => {
-				addStatus(target, "stun")
+				addStatus({target: target, status: "stun", length: 1})
 				if (chancerolled < 0.3) {
 					addStatus({target: target,status: "minor_concussion", length: 1})
 				}
