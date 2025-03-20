dad5f7acac8bb229fc4763832db484996ec77592
Glass-Memoirs <144963518+Glass-Memoirs@users.noreply.github.com>
2025-03-20 08:50:42 -0400

    Changed the on<X> function for byothwar

--- ChaosBeta.js
+++ ChaosBeta.js
@@ -1395,7 +1395,7 @@ env.STATUS_EFFECTS.byothwar = {
 	icon: "https://glass-memoirs.github.io/Chaos-beta/Images/Icons/Stupihorrible/dwaynerock.gif",
 	grantsActions: ["btgothwar"],
 	events: {
-		onAction: function({target, user, action, context}) {
+		onBeforeAction: function({target, user, action, context}) {
 			if (action.slug == "btgothwar") {
 				if (Math.random() < 0.3) {
 					context.settings.target = user
