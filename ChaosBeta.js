dc80f174bdcd627e9191cab2af981eb92b6179c9
Clocktower Collective <144963518+Glass-Memoirs@users.noreply.github.com>
2025-06-13 11:37:32 -0400

    commented out the show if, just to see if going into a fight with guarding angel works

--- ChaosBeta.js
+++ ChaosBeta.js
@@ -793,7 +793,7 @@ env.ACTOR_AUGMENTS.generic.steel_angel = {
 	description: "this is gonna be changed once i know it works",
 	alterations: [["steel_songbird","steel_angel"]],
 	component: ["utility","steel"],
-	showIf: "editingMember.augments|steel_songbird",
+	//showIf: "editingMember.augments|steel_songbird",
 	cost: 2
 }
 
