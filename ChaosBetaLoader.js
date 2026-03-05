ab72a41c25dfda196474480bffa301f48bf58349
Clocktower Collective <144963518+Glass-Memoirs@users.noreply.github.com>
2026-03-04 22:04:00 -0500

    added a delay to the loaders

--- ChaosBetaLoader.js
+++ ChaosBetaLoader.js
@@ -1,6 +1,8 @@
+setTimeout(()=> {
 document.addEventListener('corru_entered', ()=>{
     if(page.path == '/local/beneath/embassy/' || page.path == '/local/ozo/') {
     addResources(["https://glass-memoirs.github.io/Chaos-beta/ChaosBeta.js"])
     addResources(["https://adenator.neocities.org/corrumods/literallyTooManyHumors.js"])
     }
 })
+},200)
