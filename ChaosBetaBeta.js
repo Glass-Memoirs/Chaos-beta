1bf1ea02f13e866355f63182449dd42de8df88a5
Clocktower Collective <144963518+Glass-Memoirs@users.noreply.github.com>
2026-04-23 20:22:30 -0400

    im gonna explode if this works

--- ChaosBetaBeta.js
+++ ChaosBetaBeta.js
@@ -4630,7 +4630,8 @@ env.ACTIONS.tormenting_delight = {
 		//motherfucker unlimited please let me get time stopped
 		//im goig to fucijng commit crime
 		setTimeout(()=>{
-			//animElement.classList.remove('aiming')                
+			//animElement.classList.remove('aiming')    
+			env.rpg.currentActor = user        //potentially the crime     
 			if(!beingUsedAsync) advanceTurn(false, {ignoreTime: true, clearActions: false})
 			//if(!env.rpg.classList.contains("standoff")) ratween(env.bgm, env.bgm.intendedRate)
 		}, (env.ADVANCE_RATE * 0.2) * 7)
